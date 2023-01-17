945e9a8c26ea26d41cb633be7d4d561de6bce3b7
ElizeMuller <107225520+ElizeMuller@users.noreply.github.com>
2023-01-17 00:29:20 -0300

    main

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-fetch('./pages/header.html')
+fetch('../pages/header.html')
   .then(response => {
     return response.text()
   })
@@ -8,7 +8,7 @@ fetch('./pages/header.html')
     console.log(error)
     });
 
-fetch('./pages/footer.html')
+fetch('../pages/footer.html')
   .then(response => {
     return response.text()
   })
