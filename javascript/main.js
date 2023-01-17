ea57274fb089d94d493ce192bcf73ead06ca3212
haruheal <elize.muller@estudante.ifb.edu.br>
2023-01-16 23:20:43 -0300

    test2

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,22 @@
-// fetch("../pages/header.html")
-//   .then(response => {
-//     return response.text()
-//   })
-//   .then(data => {
-//     document.querySelector("header").innerHTML = data;
-//   }).catch(error => {
-//     console.log(error)
-//     });
+fetch("../pages/header.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("header").innerHTML = data;
+  }).catch(error => {
+    console.log(error)
+    });
 
-// fetch("../pages/footer.html")
-//   .then(response => {
-//     return response.text()
-//   })
-//   .then(data => {
-//     document.querySelector("footer").innerHTML = data;
-//   }).catch(error => {
-//     console.log(error)
-//     });
+fetch("../pages/footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+  }).catch(error => {
+    console.log(error)
+    });
 
 // dropdown menu
 function dropdownMenu() {
